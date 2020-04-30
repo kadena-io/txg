1b3e4e9c4bb31098854f1576f22e42392f6f8185
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2020-04-30 19:38:34 -0400

    development to 20 chains

--- lib/TXG/Utils.hs
+++ lib/TXG/Utils.hs
@@ -135,7 +135,7 @@ cidFromText t = case readEither (T.unpack t) of
 
 chainIds :: ChainwebVersion -> [ChainId]
 chainIds Mainnet01 = ChainId <$> [0..9]
-chainIds Development = ChainId <$> [0..9]
+chainIds Development = ChainId <$> [0..19]
 chainIds Testnet04 = ChainId <$> [0..9]
 
 -- -------------------------------------------------------------------------- --
@@ -306,4 +306,3 @@ mempoolMember
     -> [TransactionHash]
     -> IO (Either ClientError [Bool])
 mempoolMember m a v c txh = mempoolPost m a v c "/member" txh
-
